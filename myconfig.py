5b19528b636541a97c7343db3dc48225c064b56f
Wuwenchie <161718523+Wuwenchie@users.noreply.github.com>
2025-03-23 23:28:31 +0800

    Update myconfig.py

--- myconfig.py
+++ myconfig.py
@@ -7,6 +7,75 @@ class Mypara:
 
 
 mypara = Mypara()
+mypara.device = torch.device("cuda:1")
+# mypara.cpu_device = torch.device("cpu")  # CPU 設備
+mypara.batch_size_train = 8
+mypara.batch_size_eval = 10
+mypara.num_epochs = 40
+mypara.TFnum_epochs = 20
+mypara.TFlr = 1.5e-5
+mypara.early_stopping = True
+mypara.patience = 4
+mypara.warmup = 2000
+
+# data related
+mypara.adr_pretr = (
+    "./data/training_data/GFDL-CM4_sss_sst_1850_1976.nc"
+)
+mypara.interval = 4
+mypara.TraindataProportion = 0.9
+mypara.all_group = 13000
+mypara.adr_eval = (
+    "./data/training_data/GFDL-CM4_sss_sst_1977_2009.nc"
+)
+mypara.needtauxy = True
+mypara.input_channels = 2  # input channels = SSS + SST
+mypara.input_channal = 7  # n_lev of 3D temperature
+mypara.output_channal = 7
+mypara.input_length = 12
+mypara.output_length = 24
+# mypara.lon_range = (0, 356)
+# mypara.lat_range = (-55, 60)
+mypara.lon_range = (0, 72)  # 1E ~ 356E
+mypara.lat_range = (0, 23)  # -55S ~ 60N
+# mypara.lon_range = (-299, 59)
+# mypara.lat_range = (-55, 59)
+
+# nino34 region
+# mypara.lon_nino_relative = (26, 37) # 190E ~ 240E
+# mypara.lat_nino_relative = (10, 13)  # -5S ~ 5N
+mypara.lon_nino_relative = (26, 37)
+mypara.lat_nino_relative = (10, 13)
+
+# patch size
+mypara.patch_size = (3, 4)
+# mypara.H0 = int((mypara.lat_range[1] - mypara.lat_range[0]) / mypara.patch_size[0])
+# mypara.W0 = int((mypara.lon_range[1] - mypara.lon_range[0]) / mypara.patch_size[1])
+mypara.H0 = (23 - mypara.patch_size[0]) // mypara.patch_size[0] + 1  # 7
+mypara.W0 = (72 - mypara.patch_size[1]) // mypara.patch_size[1] + 1  # 17
+mypara.emb_spatial_size = mypara.H0 * mypara.W0
+
+# model
+mypara.model_savepath = "./model/"
+mypara.seeds = 1
+mypara.d_size = 128
+mypara.nheads = 4
+mypara.dim_feedforward = 512
+mypara.dropout = 0.2
+mypara.num_encoder_layers = 2
+mypara.num_decoder_layers = 2
+
+
+"""
+import torch
+
+
+class Mypara:
+    def __init__(self):
+        pass
+
+
+mypara = Mypara()
 mypara.device = torch.device("cuda:0")
 mypara.batch_size_train = 8
 mypara.batch_size_eval = 10
@@ -58,3 +127,4 @@ mypara.dim_feedforward = 512
 mypara.dropout = 0.2
 mypara.num_encoder_layers = 4
 mypara.num_decoder_layers = 4
+"""
